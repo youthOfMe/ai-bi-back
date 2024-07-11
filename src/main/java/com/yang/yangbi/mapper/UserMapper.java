dd00a12de75a60ce76c4e35e2cecd670bb45d6b6
youthOfMe <2040690180@qq.com>
2024-07-11 20:24:04 +0800

    feat: 初始化项目

---
+++ src/main/java/com/yang/yangbi/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.yang.yangbi.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yang.yangbi.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
